bd40da02bfd7ceb7aca0939bc8ae66bae6b7ca61
somesay1998 <42347375+somesay1998@users.noreply.github.com>
2018-08-31 19:01:11 +0800

    Update and rename config.php to config.json

--- web/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyARHJJDFtwb5wcnURwwJA3FQRrM16PF27c');
-define('GJ_CODE', 'JP');
-define('SITE_NAME', '大爷常来玩儿啊');
-define('TITLENAME', 'DouDouTube');
-define('EN2DEKEY', 'lujianbupingyishenghou');
-define('EMAIL', 'xiaofufua@protonmail.com');
-?>
